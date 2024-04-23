82daffdf307cb4cc2060b4f991d03685803f49df
RadjaShiqnals <radjagenta@gmail.com>
2024-04-23 13:11:53 +0700

    UPD : LoginSystem
    Login
    Register
    Logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            return response()->json([
+                'message' => 'Successfully logged in',
+                'name' => $user->name,
+                'email' => $user->email,
+            ]);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Successfully registered',
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        
+        return response()->json(['message' => 'Successfully logged out']);
+    }
 }
